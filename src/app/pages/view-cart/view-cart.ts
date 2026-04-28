197be2651f20a79705b14647ee6148f749f58e9a
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-04-29 00:10:38 +0530

    summarize-order final

--- src/app/pages/view-cart/view-cart.ts
+++ src/app/pages/view-cart/view-cart.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { BackButton } from '../../components/back-button/back-button';
 import { ListCartItems } from './list-cart-items/list-cart-items';
 import { TeaseWishlist } from './tease-wishlist/tease-wishlist';
+import { SummarizeOrder } from '../../components/summarize-order/summarize-order';
+import { ViewPanal } from '../../directives/view-panal';
 
 @Component({
   selector: 'app-view-cart',
-  imports: [BackButton, ListCartItems, TeaseWishlist],
+  imports: [BackButton, ListCartItems, TeaseWishlist, SummarizeOrder, ViewPanal],
   templateUrl: './view-cart.html',
   styleUrl: './view-cart.scss',
 })
