ae9d096709c9f56d1742fdac9ac44e76fa8ee973
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-04-29 23:05:49 +0530

    changes

--- src/app/pages/view-cart/view-cart.ts
+++ src/app/pages/view-cart/view-cart.ts
@@ -4,10 +4,11 @@ import { ListCartItems } from './list-cart-items/list-cart-items';
 import { TeaseWishlist } from './tease-wishlist/tease-wishlist';
 import { SummarizeOrder } from '../../components/summarize-order/summarize-order';
 import { ViewPanal } from '../../directives/view-panal';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-view-cart',
-  imports: [BackButton, ListCartItems, TeaseWishlist, SummarizeOrder, ViewPanal],
+  imports: [BackButton, ListCartItems, TeaseWishlist, SummarizeOrder, MatButtonModule],
   templateUrl: './view-cart.html',
   styleUrl: './view-cart.scss',
 })
