9857d0101c017b52997606241cce0dd52778eb12
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-05-07 19:48:04 +0530

    withStorageSync()
    create local storage using withStorageSync()

--- src/app/store/ecommerce-store.ts
+++ src/app/store/ecommerce-store.ts
@@ -16,6 +16,7 @@ import { SignInDialog } from '../components/sign-in-dialog/sign-in-dialog';
 import { SignInParams, SignUpParams, User } from '../../models/user';
 import { Router } from '@angular/router';
 import { Order } from '../../models/order';
+import { withStorageSync } from '@angular-architects/ngrx-toolkit';
 
 export type EcommerceState = {
   products: Product[];
@@ -313,6 +314,11 @@ export const EcommerceStore = signalStore(
     user: undefined,
     loading: false,
   } as EcommerceState),
+
+  withStorageSync({
+    key: 'modern-store',
+    select: ({ wishlistItems, cartItems, user }) => ({ wishlistItems, cartItems, user }),
+  }),
 
   withComputed(({ category, products, wishlistItems, cartItems }) => ({
     filteredProducts: computed(() => {
